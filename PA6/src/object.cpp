008db0745bdb4b9ac866be8b75f9d88f6b48f9ce
christopherparks-unr <christopherparks@nevada.unr.edu>
2019-10-07 12:09:30 -0700

    Updated to include more customizability, and ensure operation on ECC machines

--- PA6/src/object.cpp
+++ PA6/src/object.cpp
@@ -6,7 +6,7 @@
 
 std::vector<Scene> my_scenelist;
 
-std::string load_MTL(std::string filename, std::string meshname)
+std::string load_MTL(std::string filename, int meshIndex)
 {
   std::string true_filename;
   true_filename = filename.substr(0,filename.length()-3);
@@ -33,6 +33,7 @@ std::string load_MTL(std::string filename, std::string meshname)
 	bool within = false;
 
   std::istream reader(&fb);
+  int mesh_num = -1;
   while (reader) {
 
     line.clear();
@@ -47,11 +48,14 @@ std::string load_MTL(std::string filename, std::string meshname)
 
     if (line[0] == '\t') {line = line.substr(1,std::string::npos);}
     
-		if (line.find("newmtl ") == 0) {
-			line_extract = line.substr(7,std::string::npos);
-			if (line_extract.compare(meshname) == 0) {
-				within = true;
-			}
+		if (line.find("newmtl ") == 0)
+                {
+                  mesh_num++;
+                  if(meshIndex == mesh_num)
+                  {
+                    within = true;
+                  }
+
 		}
 
     if (within && line.find("map_Kd ") == 0) {
@@ -66,36 +70,47 @@ std::string load_MTL(std::string filename, std::string meshname)
 }
 
 //Imports the file from the scene_path argument and creates a vector of meshes from it
-Scene scene_from_assimp(std::string filename)
+Scene scene_from_assimp(std::string filename, int meshIndex, std::string textureOverride)
 {
 	Assimp::Importer importer;
 	const aiScene *readScene = importer.ReadFile(filename,aiProcess_Triangulate); // | aiProcess_JoinIdenticalVertices
 
-	Scene make;
-	make.meshes.clear();
-
-	std::string otherstring(filename);
-	make.supername = otherstring;
+	Scene createdScene;
+	createdScene.meshes.clear();
+        std::cout << "Texture Override is " << textureOverride << std::endl;
+        std::cout << "mNumMeshes is " << (int) readScene->mNumMeshes << std::endl;
+        createdScene.supername = filename;
+	//createdScene.supername = filename + "_" + std::to_string(meshIndex);
 	
-	Mesh submake;
+	Mesh wantedMesh;
 	Vertex read;
 
   //Iterate from the meshes in the scene
+        
 	for (int iter = 0; iter < (int) readScene->mNumMeshes; iter++)
 	{
-		std::string newstring(readScene->mMeshes[iter]->mName.C_Str());
-		submake.name = newstring;
+		wantedMesh.name = readScene->mMeshes[iter]->mName.C_Str();
 		
-		std::string texturepath = load_MTL(filename,newstring);
+                if(textureOverride.length() == 0 || iter != meshIndex)
+                {
+		  std::string texturepath = load_MTL(filename,iter);
+                  std::cout << "@@@@" << texturepath << std::endl;
 
-		if (texturepath.length() != 0) {
-			submake.image_data = stbi_load(texturepath.c_str(),&(submake.image_x),
-																		&(submake.image_y),&(submake.image_c),4);
-		} else {
-			submake.image_data = NULL;
-		}
+		  if (texturepath.length() != 0) {
+			  wantedMesh.image_data = stbi_load(texturepath.c_str(),&(wantedMesh.image_x),
+						&(wantedMesh.image_y),&(wantedMesh.image_c),4);
+		  } else {
+			  wantedMesh.image_data = NULL;
+		  }
+                }
+                else
+                {
+                  wantedMesh.image_data = stbi_load(textureOverride.c_str(),&(wantedMesh.image_x),
+						&(wantedMesh.image_y),&(wantedMesh.image_c),4);
+                  std::cout << "!!!!" << textureOverride << std::endl;
+                }
 
-		submake.vertex_data.clear();
+		wantedMesh.vertex_data.clear();
 
     //Get vertex info from the mesh
 		for (int verts = 0; verts < (int) (readScene->mMeshes[iter])->mNumVertices; verts++)
@@ -111,39 +126,40 @@ Scene scene_from_assimp(std::string filename)
 				read.coord[1] = (readScene->mMeshes[iter])->mTextureCoords[0][verts].y;
 			}
 
-			submake.vertex_data.push_back(read);
+			wantedMesh.vertex_data.push_back(read);
 		}
 
-		submake.indice_data.clear();
+		wantedMesh.indice_data.clear();
 
     //Get index info from the mesh
 		for (int faces = 0; faces < (int) (readScene->mMeshes[iter])->mNumFaces; faces++)
 		{
 			//ASSUMES triangle conversion worked
 
-			submake.indice_data.push_back( (readScene->mMeshes[iter])->mFaces[faces].mIndices[0] );
-			submake.indice_data.push_back( (readScene->mMeshes[iter])->mFaces[faces].mIndices[1] );
-			submake.indice_data.push_back( (readScene->mMeshes[iter])->mFaces[faces].mIndices[2] );
+			wantedMesh.indice_data.push_back( (readScene->mMeshes[iter])->mFaces[faces].mIndices[0] );
+			wantedMesh.indice_data.push_back( (readScene->mMeshes[iter])->mFaces[faces].mIndices[1] );
+			wantedMesh.indice_data.push_back( (readScene->mMeshes[iter])->mFaces[faces].mIndices[2] );
 		}
 
-		make.meshes.push_back(submake);
+		createdScene.meshes.push_back(wantedMesh);
 	}
   //Return the Scene struct, which has a name and a vector of Mesh structs, which contains the vertex and index information plus a name
-	return make;
+	return createdScene;
 }
 
 //Finds the scene object from the list of scenes (searched by path)
-Scene find_scene_from_path(std::string input)
+Scene find_scene_from_path(std::string input, int mesh_index, std::string textureOverride)
 {
   //If the scene has already been loaded, use that instead of loading it again
+/*
 	for (int iter = 0; iter < (int) my_scenelist.size(); iter++)
 	{
 		if (my_scenelist[iter].supername.compare(input) == 0) {
 			return my_scenelist[iter];
 		}
-	}
+	}*/
   //Else load the scene in from the scene_path
-	Scene make = scene_from_assimp(input);
+	Scene make = scene_from_assimp(input, mesh_index, textureOverride);
   //my_scenelist is the list of Scenes
 	my_scenelist.push_back(make);
 	return make;
@@ -153,33 +169,37 @@ Object::~Object()
 {
   Vertices.clear();
   Indices.clear();
+  my_scenelist.clear();
 }
 
-Object::Object(std::string s_p, std::string m_n, float s, float r, float r_m, float o_m, Object* p)
+Object::Object(std::string s_p, int m_i, std::string t_p, float s, float r, float r_m, float r_o, float o_m, float o_o, Object* p)
 {
-	Scene from = find_scene_from_path(s_p);
-
-	int which = from.search_mesh_by_name(m_n);
-
-	if (which == -1) {
-		printf("Invalid mesh file selected!\n");
-	}
-
-	Vertices = from.meshes[which].vertex_data;
-	Indices = from.meshes[which].indice_data;
-
-	printf("Successfully loaded %s with %i non-unique vertices and %i non-unique indices\n", m_n.c_str(), (int)Vertices.size(), (int)Indices.size());
-
-  rotation_angle = 0.0f;
-  orbit_angle = 0.0f;
-
   scene_path = s_p;
-  mesh_name = m_n;
+  mesh_index = m_i;
+  texture_path = t_p;
   scale = s;
   radius = r;
   rotation_angle_mod = r_m;
+  rotation_offset = r_o;
   orbit_angle_mod = o_m;
+  orbit_offset = o_o;
   parent = p;
+
+  rotation_angle = 0.0f + rotation_offset;
+  orbit_angle = 0.0f + orbit_offset;
+
+	Scene currentScene = find_scene_from_path(scene_path, mesh_index, texture_path);
+
+	//int which = currentScene.search_mesh_by_index(mesh_index);
+
+	if (mesh_index < 0 || mesh_index >= (int)currentScene.meshes.size()) {
+		printf("Invalid mesh file selected!\n");
+	}
+
+	Vertices = currentScene.meshes[mesh_index].vertex_data;
+	Indices = currentScene.meshes[mesh_index].indice_data;
+
+	printf("Successfully loaded %s with mesh number %i. It has %i non-unique vertices and %i non-unique indices\n", scene_path.c_str(), mesh_index, (int)Vertices.size(), (int)Indices.size());
 
   model = glm::mat4(1.0f);
 
@@ -196,12 +216,12 @@ Object::Object(std::string s_p, std::string m_n, float s, float r, float r_m, fl
 
 	has_texture = false;
 
-	if (from.meshes[which].image_data != NULL) {	
+	if (currentScene.meshes[mesh_index].image_data != NULL) {	
 		has_texture = true;
 		glGenTextures(1, &TX);
 		glBindTexture(GL_TEXTURE_2D,TX);
-		glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,from.meshes[which].image_x,from.meshes[which].image_y,
-									0,GL_RGBA,GL_UNSIGNED_BYTE,from.meshes[which].image_data);
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,currentScene.meshes[mesh_index].image_x,currentScene.meshes[mesh_index].image_y,
+									0,GL_RGBA,GL_UNSIGNED_BYTE,currentScene.meshes[mesh_index].image_data);
 		glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
 		glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
 	}
